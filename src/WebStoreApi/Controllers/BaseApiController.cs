98f53abc47e145902179f7066b84a1fadddf89e9
Tihomir Vasilev <rinzz86@yahoo.com>
2022-02-03 14:48:20 +0200

    API: Initial Create

---
+++ src/WebStoreApi/Controllers/BaseApiController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStoreApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+    }
+}
